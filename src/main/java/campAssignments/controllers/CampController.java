ac29c9468f92fa34ce9e6bcbbf94621f1d4edbcc
Bert J Zylstra <bert.zylstra65@gmail.com>
2017-09-28 15:17:27 -0600

    Enable Cross Site to Editor App
    - CrossOrigin on Camp controller

--- src/main/java/campAssignments/controllers/CampController.java
+++ src/main/java/campAssignments/controllers/CampController.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ import campAssignmentsApi.Camp;
  * @author Bert
  * Controller to fetch Camp identification object
  */
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class CampController {
 
